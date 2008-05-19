ac07b58cc8c21631e63168753126092e8ffbfdaf
Christian Grobmeier <grobmeier@gmail.com>
2008-05-19 04:18:00 +0000

    new errorcode for security/params

--- piwi/lib/piwi/PiwiException.class.php
+++ piwi/lib/piwi/PiwiException.class.php
@@ -3,6 +3,7 @@
 class PiwiException extends Exception {
 	
 	const ERR_NO_XML_DEFINITION    = 1001;
+	const ERR_PARAM_NOT_EXPECTED   = 1002;
     
     public function __construct($message ="An exception occured", $code = 1) {
         parent::__construct($message, $code);
