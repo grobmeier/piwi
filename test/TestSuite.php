c4a5b2b956aaa5eb78c5dd69d93b8242214be40f
Daniel Palme <danielpalme82@googlemail.com>
2008-11-21 16:57:04 +0000

    Added webtests

--- test/TestSuite.php
+++ test/TestSuite.php
@@ -1,7 +1,11 @@
 <?php
+  // Classes
   $this->addFile('test/lib/piwi/connector/ConnectorFactoryTest.php');
   $this->addFile('test/lib/piwi/connector/SQLite2ConnectorTest.php');  
   $this->addFile('test/lib/piwi/connector/SQLite3ConnectorTest.php');
-    $this->addFile('test/lib/piwi/generator/GeneratorFactoryTest.php');
+  $this->addFile('test/lib/piwi/generator/GeneratorFactoryTest.php');
   $this->addFile('test/lib/piwi/forms/FormFactoryTest.php');
+  
+  // Webtest
+  $this->addFile('test/web/FormsTest.php');
 ?>
