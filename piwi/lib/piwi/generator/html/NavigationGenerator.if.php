e2e13134c8e4c31a7968808328d9df3dae22dfe6
Daniel Palme <danielpalme82@googlemail.com>
2008-12-04 14:36:11 +0000

    Refactored NavigationGenerator.if.php

--- piwi/lib/piwi/generator/html/NavigationGenerator.if.php
+++ piwi/lib/piwi/generator/html/NavigationGenerator.if.php
@@ -9,12 +9,5 @@ interface NavigationGenerator {
 	 * @return string The navigation as HTML.
 	 */
 	public function generate(array $siteElements = null);
-	
-	/**
-	 * Used to pass parameters to the Generator.
-	 * @param string $key The name of the parameter.
-	 * @param string $value The value of the parameter.
-	 */
-	public function setProperty($key, $value);
 }
 ?>
--- piwi/lib/piwi/generator/html/SiteMapPathNavigationGenerator.class.php
+++ piwi/lib/piwi/generator/html/SiteMapPathNavigationGenerator.class.php
@@ -30,13 +30,5 @@ class SiteMapPathNavigationGenerator implements NavigationGenerator {
 		
 		return $siteMapPathHTML;
 	}
-	
-    /**
-	 * Used to pass parameters to the Generator.
-	 * @param string $key The name of the parameter.
-	 * @param string $value The value of the parameter.
-	 */
-    public function setProperty($key, $value) {    	
-    }
 }
 ?>
