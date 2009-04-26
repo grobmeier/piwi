26345fcdcba0f4d968c861716a96d67703cc474e
Daniel Palme <danielpalme82@googlemail.com>
2009-04-26 11:19:55 +0000

    Continued fixing of tests

--- test/TestSuite.php
+++ test/TestSuite.php
@@ -1,34 +1,34 @@
 <?php
   // Classes
-//  $this->addFile('test/unit/lib/piwi/beanfactory/BeanFactoryTest.php');
-//  
-//  $this->addFile('test/unit/lib/piwi/connector/ConnectorFactoryTest.php');
-//  $this->addFile('test/unit/lib/piwi/connector/SQLite2ConnectorTest.php');  
-//  $this->addFile('test/unit/lib/piwi/connector/SQLite3ConnectorTest.php');
-//  
-//  $this->addFile('test/unit/lib/piwi/generator/GeneratorFactoryTest.php');
-//  
-//  $this->addFile('test/unit/lib/piwi/forms/FormFactoryTest.php');
-//
-//  $this->addFile('test/unit/lib/piwi/io/FileTest.php');
-//  $this->addFile('test/unit/lib/piwi/io/FolderTest.php');  
-//  
-//  $this->addFile('test/unit/lib/piwi/media/AlbumTest.php');
-//  $this->addFile('test/unit/lib/piwi/media/MediaCenterTest.php');  
-//  
-//  $this->addFile('test/unit/lib/piwi/pager/PagerTest.php');
+  $this->addFile('test/unit/lib/piwi/beanfactory/BeanFactoryTest.php');
+  
+  $this->addFile('test/unit/lib/piwi/connector/ConnectorFactoryTest.php');
+  $this->addFile('test/unit/lib/piwi/connector/SQLite2ConnectorTest.php');  
+  $this->addFile('test/unit/lib/piwi/connector/SQLite3ConnectorTest.php');
+  
+  $this->addFile('test/unit/lib/piwi/generator/GeneratorFactoryTest.php');
+  
+  $this->addFile('test/unit/lib/piwi/forms/FormFactoryTest.php');
 
-//$this->addFile('test/unit/lib/piwi/page-processing/XMLPageTest.php');
+  $this->addFile('test/unit/lib/piwi/io/FileTest.php');
+  $this->addFile('test/unit/lib/piwi/io/FolderTest.php');  
+  
+  $this->addFile('test/unit/lib/piwi/media/AlbumTest.php');
+  $this->addFile('test/unit/lib/piwi/media/MediaCenterTest.php');  
+  
+  $this->addFile('test/unit/lib/piwi/pager/PagerTest.php');
+
+  $this->addFile('test/unit/lib/piwi/page-processing/XMLPageTest.php');
   $this->addFile('test/unit/lib/piwi/page-processing/SiteTest.php');
-//  $this->addFile('test/unit/lib/piwi/page-processing/SiteMapHelperTest.php');    
-//  $this->addFile('test/unit/lib/piwi/page-processing/ConfigurationManagerTest.php');
-//
-//  // Webtest
-//  $this->addFile('test/web/FormsTest.php');  
-//  $this->addFile('test/web/AuthenticationTest.php');
-//  $this->addFile('test/web/DatabaseTest.php');
-//  $this->addFile('test/web/DataGridTest.php');
-//  $this->addFile('test/web/FormsTest.php');
-//  $this->addFile('test/web/LocalizationTest.php');
-//  $this->addFile('test/web/ErrorPageTest.php');  
+  $this->addFile('test/unit/lib/piwi/page-processing/SiteMapHelperTest.php');    
+  $this->addFile('test/unit/lib/piwi/page-processing/ConfigurationManagerTest.php');
+
+  // Webtest
+  $this->addFile('test/web/FormsTest.php');  
+  $this->addFile('test/web/AuthenticationTest.php');
+  $this->addFile('test/web/DatabaseTest.php');
+  $this->addFile('test/web/DataGridTest.php');
+  $this->addFile('test/web/FormsTest.php');
+  $this->addFile('test/web/LocalizationTest.php');
+  $this->addFile('test/web/ErrorPageTest.php');  
 ?>
