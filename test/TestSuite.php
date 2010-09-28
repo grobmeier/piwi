dec10f9e2da21749f871cd8ab05f30fd4241f7f3
Christian Grobmeier <grobmeier@gmail.com>
2010-09-28 11:31:47 +0000

    cleaned up line breaks

--- test/TestSuite.php
+++ test/TestSuite.php
@@ -1,28 +1,20 @@
 <?php
   // Classes
   $this->addFile('test/unit/lib/piwi/beanfactory/BeanFactoryTest.php');
-  
   $this->addFile('test/unit/lib/piwi/connector/ConnectorFactoryTest.php');
   $this->addFile('test/unit/lib/piwi/connector/SQLite2ConnectorTest.php');  
   $this->addFile('test/unit/lib/piwi/connector/SQLite3ConnectorTest.php');
-  
   $this->addFile('test/unit/lib/piwi/generator/GeneratorFactoryTest.php');
-  
   $this->addFile('test/unit/lib/piwi/forms/FormFactoryTest.php');
-
   $this->addFile('test/unit/lib/piwi/io/FileTest.php');
   $this->addFile('test/unit/lib/piwi/io/FolderTest.php');  
-  
   $this->addFile('test/unit/lib/piwi/media/AlbumTest.php');
   $this->addFile('test/unit/lib/piwi/media/MediaCenterTest.php');  
-  
   $this->addFile('test/unit/lib/piwi/pager/PagerTest.php');
-
-  $this->addFile('test/unit/lib/piwi/page-processing/XMLPageTest.php');
   $this->addFile('test/unit/lib/piwi/page-processing/SiteTest.php');
-  $this->addFile('test/unit/lib/piwi/page-processing/SiteMapHelperTest.php');    
+  $this->addFile('test/unit/lib/piwi/page-processing/XMLPageTest.php');
+  $this->addFile('test/unit/lib/piwi/page-processing/SiteMapHelperTest.php');     
   $this->addFile('test/unit/lib/piwi/page-processing/ConfigurationManagerTest.php');
-
   $this->addFile('test/unit/lib/piwi/inputprovider/streams/StreamManagerTest.php');
   
   // Webtest
@@ -32,5 +24,5 @@
   $this->addFile('test/web/DataGridTest.php');
   $this->addFile('test/web/FormsTest.php');
   $this->addFile('test/web/LocalizationTest.php');
-  $this->addFile('test/web/ErrorPageTest.php');  
+  $this->addFile('test/web/ErrorPageTest.php');
 ?>
